8337509d14b945d2a3fe0f489612e10ab1a0589d
Ali <ali.shiekh16@outlook.com>
2025-08-14 03:44:14 +0500

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,8 @@ const workSans = Work_Sans({
 });
 
 export const metadata: Metadata = {
-  title: 'Orbit',
-  description: 'Orbit platform.',
+  title: 'Orbit - Activity Marketplace',
+  description: 'Orbit where you find the activity you love.',
 };
 
 export default function RootLayout({
